5f66fa3b9fa0ab86a5a5fb7d6af2e99dd5fe9616
sasaa3865 <sasaa3865@gmail.com>
2025-02-05 18:21:00 +0900

    리액트 접근포트 변경

--- src/main/react/vite.config.ts
+++ src/main/react/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     host: '0.0.0.0',  // 외부에서 접근할 수 있도록 설정
-    port: 80,  // 원하는 포트 번호로 변경
+    port: 3001,  // 원하는 포트 번호로 변경
   },
   plugins: [react()],
 })
