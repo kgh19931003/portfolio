9d39c7bc8aefa9033f4c467309bb04b2d6a5b019
sasaa3865 <sasaa3865@gmail.com>
2025-02-12 20:28:31 +0900

    전자정부 프레임워크 이동 버튼 추가

--- src/main/react/src/pages/kotlin.tsx
+++ src/main/react/src/pages/kotlin.tsx
@@ -61,6 +61,11 @@ const kotlin: React.FC = () => {
                 }}/>
             </div>
 
+            <div className="text-left fixed w-72 top-24 left-0 py-0 px-4 rounded-lg z-50 ">
+                <Button label="전자정부 프레임워크" bg="bg-blue-500" onClick={() => {
+                    window.open("http://kim-portfolio.p-e.kr:8080", "_blank", "noopener,noreferrer");
+                }}/>
+            </div>
 
             {/* 개인정보 섹션 */}
             <div className="py-10 bg-white shadow-2xl rounded-xl max-w-4xl mx-auto mt-10">
